212b267429f8aeb022f4b161ae1993a4e08fd3bf
Arthur C Frederico <arthur.c.fred@hotmail.com>
2024-01-28 12:02:04 -0300

    organizing 1 JS

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const form = document.getElementById('form-atividade');
-const imgAprovado = '<img src="./images/aprovado.png" alt="emoji celebrando" />';
-const imgReprovado = '<img src="./images/reprovado.png" alt="emoji decepcionado" />';
+const imgAprovado = '<img src="./images/aprovado.png" alt="emoji celebrando" />'
+const imgReprovado = '<img src="./images/reprovado.png" alt="emoji decepcionado" />'
+const atividades = [];
+const notas = [];
 
 let linhas = '';
 
@@ -9,16 +11,20 @@ form.addEventListener('submit', function(e) {
 
     adicionaLinha();
     atualizaTabela();
-});
+
+})
 
 function adicionaLinha() {
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
+    atividades.push(inputNomeAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value));
+
     let linhas = '<tr>';
     linha += `<td>${inputNomeAtividade.value}</td>`;
     linha += `<td>${inputNotaAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}}</td>`;
     linha += '</tr>';
 
     linhas += linha;
@@ -30,4 +36,16 @@ function adicionaLinha() {
 function atualizaTabela() {
     const corpoTabela = document.querySelector('tbody');
     corpoTabela.innerHTML = linhas;
+}
+
+function atualizaMediaFinal() {
+    let somadasNotas = 0;
+
+    for (let i=0; i < notas.length; i++) {
+        somadasNotas += notas[i];
+    }
+
+    const media = somadasNotas / notas.length;
+
+    console.log(media);
 }

493416acf17e3871e3cb83aec213ab816e220466
ArthurCFred <154742064+ArthurCFred@users.noreply.github.com>
2024-03-25 12:59:33 -0300

    adjustment_main.js

--- main.js
+++ main.js
@@ -3,28 +3,26 @@ let campoA = document.getElementById("campo-a");
 let campoB = document.getElementById("campo-b");
 
 form.addEventListener("submit", function(e){
-
     e.preventDefault();
 
     const mensagemResposta = document.getElementById("mensagem-resposta")
-    const mensagemCasoB = document.getElementById("casoB")
-    const mensagemCasoA = document.getElementById("casoA")
-    const mensagemCasoIgual = document.getElementById("casoIgual")
 
     if(campoB.value > campoA.value)
     {
-        mensagemResposta.innerHTML = mensagemCasoB;
-        mensagemResposta.style.display = "block";
+        mensagemResposta.innerHTML = "O valor do campo B é maior que o valor do campo A";
+        mensagemResposta.style.color = "red";
     }
 
-else if(campoB.value === campoA.value)
-{
-    mensagemResposta.innerHTML = mensagemCasoIgual;
-    mensagemResposta.style.display = "block";
-}
-else(campoA.value > campoB.value)
-{
-    mensagemResposta.innerHTML = mensagemCasoA;
-    mensagemResposta.style.display = "block";
-}
+    else if(campoB.value === campoA.value)
+    {
+        mensagemResposta.innerHTML = "O valor dos campos são iguais!";
+        mensagemResposta.style.color = "black";
+    }
+    else
+    {
+        mensagemResposta.innerHTML = "O valor do campo A é maior que o valor do campo B";
+        mensagemResposta.style.color = "blue";
+    }
+    
+     mensagemResposta.style.display = "block";    
 })
